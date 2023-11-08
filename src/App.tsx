78651a32c1bf871d4a04d4d645d6715d6e363562
JintoYamanaka <yamajin.ips11@gmail.com>
2023-11-09 01:14:44 +0900

    パーツをコンポーネント化

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { useState, useEffect } from "react";
+import { css } from "@emotion/react";
+import InputField from "./components/InputField";
+import Button from "./components/Button";
+
+const appStyle = css`
+  text-align: center;
+  margin-top: 50px;
+`;
 
 const App = () => {
-  const [minutes, setMinutes] = useState<string>('0');
-  const [seconds, setSeconds] = useState<string>('0');
+  const [minutes, setMinutes] = useState<string>("0");
+  const [seconds, setSeconds] = useState<string>("0");
   const [totalSeconds, setTotalSeconds] = useState<number>(0);
   const [isActive, setIsActive] = useState<boolean>(false);
   const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
+    let id: NodeJS.Timeout | null = null;
     if (isActive && totalSeconds > 0) {
-      const id = setTimeout(() => {
-        setTotalSeconds(totalSeconds - 1);
+      id = setInterval(() => {
+        setTotalSeconds((seconds) => seconds - 1);
       }, 1000);
       setIntervalId(id);
     } else if (totalSeconds === 0 && isActive) {
       clearInterval(intervalId as NodeJS.Timeout);
-      alert('Time’s up!'); // 実際には効果音を鳴らす
-      setIsActive(false); // タイマーを非アクティブ状態
+      alert("Time’s up!"); // ここで効果音を再生する
+      setIsActive(false);
     }
-    return () => clearInterval(intervalId as NodeJS.Timeout);
+    return () => {
+      if (id) clearInterval(id);
+    };
   }, [isActive, totalSeconds]);
 
   const toggleActive = () => {
@@ -28,11 +39,11 @@ const App = () => {
 
   const handleStart = () => {
     const totalSec = parseInt(minutes) * 60 + parseInt(seconds);
-    if (!isNaN(totalSec) && totalSec > 0) {
+    if (!isNaN(totalSec) && totalSec >= 0) {
       setTotalSeconds(totalSec);
       setIsActive(true);
     } else {
-      alert('Invalid time input!');
+      alert("Invalid time input!");
     }
   };
 
@@ -42,33 +53,40 @@ const App = () => {
   };
 
   const handleMinutesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (parseInt(e.target.value) < 60) {
-      setMinutes(e.target.value);
-    } else {
-      alert('Minutes should be less than 60.');
-    }
+    setMinutes(e.target.value);
   };
 
   const handleSecondsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (parseInt(e.target.value) < 60) {
-      setSeconds(e.target.value);
-    } else {
-      alert('Seconds should be less than 60.');
-    }
+    setSeconds(e.target.value);
   };
 
   return (
-    <div className="App">
+    <div css={appStyle}>
       <h1>Timer App</h1>
-      <input type="number" value={minutes} onChange={handleMinutesChange} placeholder="Minutes" />
-      <input type="number" value={seconds} onChange={handleSecondsChange} placeholder="Seconds" />
-      <button onClick={handleStart} disabled={isActive}>Start</button>
-      <button onClick={toggleActive} disabled={!isActive && totalSeconds === 0}>
-        {isActive ? 'Pause' : 'Resume'}
-      </button>
-      <button onClick={handleReset}>Reset</button>
+      <InputField
+        label="Minutes:"
+        value={minutes}
+        onChange={handleMinutesChange}
+      />
+      <InputField
+        label="Seconds:"
+        value={seconds}
+        onChange={handleSecondsChange}
+      />
+      <Button onClick={handleStart} disabled={isActive} text="Start" />
+      <Button
+        onClick={toggleActive}
+        disabled={!isActive && totalSeconds === 0}
+        text={isActive ? "Pause" : "Resume"}
+      />
+      <Button
+        onClick={handleReset}
+        disabled={!isActive && totalSeconds === 0}
+        text="Reset"
+      />
       <h2>
-        Time Remaining: {Math.floor(totalSeconds / 60)}:{totalSeconds % 60 < 10 ? `0${totalSeconds % 60}` : totalSeconds % 60}
+        Time Remaining: {Math.floor(totalSeconds / 60)}:
+        {totalSeconds % 60 < 10 ? `0${totalSeconds % 60}` : totalSeconds % 60}
       </h2>
     </div>
   );
